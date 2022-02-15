0918604d2a886c800361e562b935599dfb28230f
Normand Terceros <normandlaredo@gmail.com>
2022-02-15 13:58:39 +0000

    adding model and view base

---
+++ qweb_module/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Module Qweb",
+    'summary': "This module is to practice about template Qweb on Odoo 15",
+    'author': "Nor-Mand",
+    'depend':['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/myqweb_views.xml',
+        'views/myqweb_menu.xml',
+    ],
+}
